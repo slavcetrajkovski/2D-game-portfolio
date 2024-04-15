861d06c2efd4876be3acb188378b474515be70a8
Slavcho <s.trajkovski02@yahoo.com>
2024-04-15 17:25:56 +0200

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is where the magic happens. I work mostly in Java, SpringBoot and React these days.
+      But, I decided to make a cool 2D game portfolio with only using HTML, CSS and JavaScript.
+      <br>Anyway, regardless of the language, I just love programming.
+      Here is my <a href="https://github.com/slavcetrajkovski" target="_blank">Github</a>!<br>
+      <b>Hint:</b> My resume is on the desk in the other room!`,
+    "cs-degree": `This is suppose to be my CS degree. Currently working on it, I'm in my 3rd year of studies.`,
+    "sofa-table": `That's my sofa. I like to relax here and prepare my classes for some of my students.<br>
+    You are probably confused..."What does he mean by my students?" Go in the other room, the explanation is on the desk.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently.`,
+    bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. You can download it by clicking <a href="https://github.com/slavcetrajkovski/portfolio/raw/main/CV.pdf" target="_blank" download="CV.pdf">here</a>.
+    <br>Not a fan of downloading things from strangers? Got ya, you can find it on my <a href="https://github.com/slavcetrajkovski/portfolio/blob/main/CV.pdf" target="_blank">Github</a>.`,
+    projects: `Info about this 2D game portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `This is my shelve. I am a passionate collector of figurines and comic books. I also made an e-commerce website related to that.
+    <a href="https://www.funkogram.mk/" target="_blank">Check it out?</a><br>
+    <b>Hint:</b> If for some reason the link doesn't work, go on the desk on your left, you'll find it there on my resume!`,
+  };
